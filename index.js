18602d4c8e1d2bb52134ace5bdb2b608443327f3
anstimlex <anstimlex@yahoo.com>
2017-09-22 10:40:32 +0800

    sadsad

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-
-
-
 alert("welcome")
 
-var student1=yatim
-var student2=mengLai
+var student1 = yatim
+var student2 = mengLai
 
 
 alert("names are:" + student1 + "and " + student2)
 
 
 function () {
-    
+
 }
 
 
@@ -49,7 +46,7 @@ var student_2 = "ken"
 
 
 
-var welcome_msg = "Hai. We are friends.. " +  student_1 + " , " + student_2
+var welcome_msg = "Hai. We are friends.. " + student_1 + " , " + student_2
 
 console.log(welcome_msg)
 
@@ -65,21 +62,21 @@ function greet(nameOfPerson) {
 
 
 
-    console.log( greeting_message )
+    console.log(greeting_message)
 
 }
 
 
 
-greet( "Bala " )
+greet("Bala ")
 
 
 
-greet( "Zin min" )
+greet("Zin min")
 
 
 
-greet( "Kenneth" )
+greet("Kenneth")
 
 
 
@@ -117,7 +114,7 @@ If i pass "bala" and "ken" as inputs to the funtion, it should return
 
 
 
-*/ 
+*/
 
 
 
@@ -129,7 +126,7 @@ function doubleTheGivenName(name) {
 
 
 
-function joinNames( name1, name2 ) {
+function joinNames(name1, name2) {
 
     // This function is going to join two names ( name1, name2)
 
@@ -143,7 +140,7 @@ function joinNames( name1, name2 ) {
 
     // var advanced_string = name1 + " " + name1  + " | " +  name2 + " " + name2
 
-    var advanced_string = doubleTheGivenName(name1)  + " | " +  doubleTheGivenName(name2) 
+    var advanced_string = doubleTheGivenName(name1) + " | " + doubleTheGivenName(name2)
 
     return advanced_string
 
@@ -151,17 +148,17 @@ function joinNames( name1, name2 ) {
 
 
 
-console.log( joinNames("Bala", "Ken")  )
+console.log(joinNames("Bala", "Ken"))
 
 
 
 
 
-function add(a,b) {
+function add(a, b) {
 
-    console.log(a+b)
+    console.log(a + b)
 
-    return a + b 
+    return a + b
 
 }
 
@@ -196,29 +193,51 @@ function processInput() {
 function doubleTheGivenName(name) {
     var i = 100
     while (i > 1) {
-        console.log (i + " bottles of beer on the wall," + i + " bottles of beer.")
-        
-        console.log ("Take one down and pass it around," + --i + " bottles of beer on the wall.")    
+        console.log(i + " bottles of beer on the wall," + i + " bottles of beer.")
+
+        console.log("Take one down and pass it around," + --i + " bottles of beer on the wall.")
     }
-        if (i == 1) {
-            console.log (i + " bottles of beer on the wall," + i + " bottles of beer.")
-            console.log ("Go to the store and buy some more, 99 bottles of beer on the wall.")
+    if (i == 1) {
+        console.log(i + " bottles of beer on the wall," + i + " bottles of beer.")
+        console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
     }
 
 
-    var i = 100
+    var i = 99
     while (i > 0) {
-        console.log (i + " bottles of beer on the wall," + i-- + " bottles of beer.")
-        if (i > 1) {        
-        console.log ("Take one down and pass it around," + i + " bottles of beer on the wall.")    
+        console.log(i + " bottles of beer on the wall," + i-- + " bottles of beer.")
+        if (i > 0) {
+            console.log("Take one down and pass it around," + i + " bottles of beer on the wall.")
+        } else
+            console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    }
+
+
+
+    Objects
+    var student = {
+        name: "Ken",
+        age: 20,
+        marks: [80, 89, 67, 90, 100],
+        subjects: ["Math", "CS", "EC", "Math2", "Math3"],
+        avg: 0,
+        young: false,
+        welcome_function: function () {
+            console.log("WELCOME BLA BLA BLA..")
+        },
+        bmi: {
+            height: 200,
+            weight: 70
         }
     }
-        console.log ("Go to the store and buy some more, 99 bottles of beer on the wall.");
-    
 
+    // Access the value of name "property" from student object ?? 
 
+    console.log(student.name) var mymarks_list = student.marks
+    var no_of_items = mymarks_list.length
+    var index = 0
+    var total = 0
+    while (index < no_of_items) {
+        total = total + mymarks_list[index] index++
     }
-        if (i == 1) {
-            console.log (i + " bottles of beer on the wall," + i + " bottles of beer.")
-            console.log ("Go to the store and buy some more, 99 bottles of beer on the wall.")
-    }
+    var average = total / no_of_items student.avg = average
